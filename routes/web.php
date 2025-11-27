cc9473c72010fc954e327c31da01ced2ec1a833e
esteves <otaviob925@gmail.com>
2025-11-27 13:40:41 -0300

    Login Previous

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,14 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::get('/fix-password', function () {
+    $user = \App\Models\User::where('email', 'admin@admin.com')->first();
+    if (!$user) return 'Usuário Admin não encontrado!';
+
+    $user->password = \Illuminate\Support\Facades\Hash::make('password');
+    $user->save();
+
+    return 'Senha corrigida com sucesso!';
+});
+
 require __DIR__.'/auth.php';
